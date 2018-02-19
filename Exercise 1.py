6a2753a6cb7fc0a06d318d33691b3c4cef333c4e
chezkaviquiera <36612260+chezkaviquiera@users.noreply.github.com>
2018-02-19 12:43:29 +0800

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,5 @@
+money=input("How much money do you have? Php")
+print"You have Php",money
+iPhone=input("Enter price of iPhone: ")
+print"You can afford",money/iPhone, "iPhone."
+print"You need Php",iPhone-money,"more."
